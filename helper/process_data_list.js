01608680a3b044f2a7981ccc7e04f815c258ab6c
Vishal Singh <bs08081997@gmail.com>
2026-04-03 02:17:53 +0530

    updating env name

--- helper/process_data_list.js
+++ helper/process_data_list.js
@@ -24,7 +24,7 @@ const process_email_send = async (
   email_body,
   client,
   client_uri,
-  automation_name
+  automation_name,
 ) => {
   try {
     // fetch topic for email and create a transporter for sender email
@@ -48,9 +48,9 @@ const process_email_send = async (
         // <a href="<%= base_url %>?uid=<%= uuid %>&redirect=https://actual-link.com" target="_blank">Click here to view updates</a>
         const email_data = {
           name: client_name,
-          base_url: process.env.SERVER_BASE_URL,
+          base_url: process.env.FRONTEND_URL,
           uuid: client_uuid,
-          unsubscribe_link: `${process.env.SERVER_BASE_URL}/Account/${client_uuid}/UnsubscribeEmail`,
+          unsubscribe_link: `${process.env.FRONTEND_URL}/Account/${client_uuid}/UnsubscribeEmail`,
           company_name: client.company_name ? client.company_name : "",
         };
         const emailHtml = ejs.render(email_body[0].body, email_data);
@@ -89,7 +89,7 @@ const process_email_send = async (
           ["Person Email"]: client_email_id,
         },
         "email_send_on",
-        new Date()
+        new Date(),
       );
       await new Promise((resolve) => setTimeout(resolve, 10000));
     }
@@ -101,16 +101,16 @@ const process_email_send = async (
 const filter_sender_list_and_update = async (
   sender_list,
   remove_emailUser,
-  sender_list_uri
+  sender_list_uri,
 ) => {
   // Filter out the sender to remove - this is done synchronously
   const updatedSenders = sender_list.filter(
-    (sender) => sender.emailUser !== remove_emailUser
+    (sender) => sender.emailUser !== remove_emailUser,
   );
 
   // Find the sender to update
   const senderToUpdate = sender_list.find(
-    (sender) => sender.emailUser === remove_emailUser
+    (sender) => sender.emailUser === remove_emailUser,
   );
 
   if (senderToUpdate) {
@@ -118,7 +118,7 @@ const filter_sender_list_and_update = async (
     let nextDay = new Date(
       now.getFullYear(),
       now.getMonth(),
-      now.getDate() + 1
+      now.getDate() + 1,
     );
 
     // Perform asynchronous operations separately (don't use filter)
@@ -130,7 +130,7 @@ const filter_sender_list_and_update = async (
         emailPassword: senderToUpdate.emailPassword,
       },
       "total_email_sent",
-      0
+      0,
     );
     await new Promise((resolve) => setTimeout(resolve, 5000));
 
@@ -142,7 +142,7 @@ const filter_sender_list_and_update = async (
         emailPassword: senderToUpdate.emailPassword,
       },
       "limit_reset_on",
-      nextDay
+      nextDay,
     );
   }
 
@@ -194,17 +194,17 @@ exports.process_data = async (automation_data) => {
         // if we have validsender length then fetch the contact details and slice the list with total no of balance email
         let clients = await sheetsHelper.getSheetData(contact_list_uri);
         const clients_to_send_email = clients.filter(
-          (client) => client && !client.email_send_on
+          (client) => client && !client.email_send_on,
         );
         // slicing the list
         const limited_clients = clients_to_send_email.slice(
           0,
-          total_email_limit
+          total_email_limit,
         );
 
         for (let client of limited_clients) {
           let randomSenderIndex = Math.floor(
-            Math.random() * validSenders.length
+            Math.random() * validSenders.length,
           );
           let sender = validSenders[randomSenderIndex];
 
@@ -215,7 +215,7 @@ exports.process_data = async (automation_data) => {
             email_body,
             client,
             contact_list_uri,
-            automation_name
+            automation_name,
           );
 
           // first check wheather the limit and total_email sent is equal or not
@@ -232,7 +232,7 @@ exports.process_data = async (automation_data) => {
             validSenders = await filter_sender_list_and_update(
               validSenders,
               sender.emailUser,
-              sender_email_uri
+              sender_email_uri,
             );
             // remove the completed users from bucket list as well
             delete sender_email_bucket_with_limit[sender.emailUser];
@@ -254,7 +254,7 @@ exports.process_data = async (automation_data) => {
                 sender_email_uri,
                 matcher,
                 "total_email_sent",
-                total_email_sent
+                total_email_sent,
               );
             }
           }
