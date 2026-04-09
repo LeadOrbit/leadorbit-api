eb7ae9f77357a717630d2c9e1c8c4eb2cbf028ee
Vishal Singh <bs08081997@gmail.com>
2026-04-10 02:29:26 +0530

    chore(config): add pooling config to db config and update CORS origin

--- config/db.config.js
+++ config/db.config.js
@@ -1,17 +1,26 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: "postgres",
   logging: process.env.NODE_ENV === "development" ? console.log : false,
+
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false,
     },
   },
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000, // wait 30 sec before timeout
+    idle: 10000,
+  },
+
   retry: {
     max: 5,
-    timeout: 5000,
   },
 });
 
